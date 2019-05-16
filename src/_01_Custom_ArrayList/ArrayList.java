e1b5f3352ac3c51414640dcf09e78f6284ee0948
ilabstaff <ilabstaff@172.16.116.76>
2019-05-15 18:23:38 -0700

    worked on custom arraylisy

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,17 +3,19 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
-	
+	private T[] list;
+	int num = 0;
 	public ArrayList() {
+		list = (T[])new Object[num];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return list[loc];
 	}
 	
 	public void add(T val) {
-		
+		list= (T[])new Object[num+1];
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
