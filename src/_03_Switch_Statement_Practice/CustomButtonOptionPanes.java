9e0706b70c73d0189ed40158ff42075ec11e6602
ilabstaff <ilabstaff@172.16.116.69>
2019-07-10 18:24:09 -0700

    Finished CustumButtonOptonPanes

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -11,7 +11,32 @@ public class CustomButtonOptionPanes {
 
 		String choice = options[input];
 		
+		switch(choice) {
+		case "Sunday":
+			System.out.println("Go to church and get kicked out of Applebee's");
+			break;
+		case "Monday":
+			System.out.println("Complain about your hangover and Minecraft");
+			break;
+		case "Tuesday":
+			System.out.println("TAAAACCCCCCCOOOOOOO TUUUUUUUUESSDAAAAAYY");
+			break;
+		case "Wednesday":
+			System.out.println("FROG and Spider-man");
+			break;
+		case "Thursday":
+			System.out.println("Complain your team should've been at the playoffs");
+			break;
+		case "Friday":
+			System.out.println("Finally you can stay home alone or waste your entire minimunm wage check on fancy drinks");
+			break;
+		case "Saturday":
+			System.out.println("Friday but better");
+			break;
+		default:
+			System.out.println("Pizza Time");
+		}
+		}
 		//use a switch statement to do something cool for each option
 		
 	}
-}
